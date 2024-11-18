356aed331d2341589cc41ad819907d907830f84e
Jonath-z <jonathanzihindula95@gmail.com>
2024-11-18 10:37:09 +0200

    feat: open the login window in the popup

--- src/hooks/useIcpAuth.ts
+++ src/hooks/useIcpAuth.ts
@@ -2,7 +2,7 @@ import { canisterId, createActor } from "@/utilities/icp/issuerFactory";
 import { Identity } from "@dfinity/agent";
 import { AuthClient } from "@dfinity/auth-client";
 import { Principal } from "@dfinity/principal";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 type Auth = {
   client: AuthClient;
@@ -47,16 +47,32 @@ const useIcpAuth = () => {
     initializeContract();
   }, []);
 
+  const popupCenter = useCallback((): string | undefined => {
+    const AUTH_POPUP_WIDTH = 576;
+    const AUTH_POPUP_HEIGHT = 625;
+
+    if (typeof window === "undefined" || !window.top) {
+      return undefined;
+    }
+
+    const { innerWidth, innerHeight, screenX, screenY } = window;
+
+    const y = innerHeight / 2 + screenY - AUTH_POPUP_HEIGHT / 2;
+    const x = innerWidth / 2 + screenX - AUTH_POPUP_WIDTH / 2;
+
+    return `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=${AUTH_POPUP_WIDTH}, height=${AUTH_POPUP_HEIGHT}, top=${y}, left=${x}`;
+  }, []);
+
   async function login(callback: (principal: string) => void) {
     if (!window) return;
     const authClient = window.auth.client;
 
     const isAuthenticated = await authClient.isAuthenticated();
-    console.log({ isAuthenticated });
     if (!isAuthenticated) {
       await authClient?.login({
         maxTimeToLive: MAX_TTL,
         identityProvider: IDENTITY_PROVIDER,
+        windowOpenerFeatures: popupCenter(),
         onSuccess: async () => {
           window.auth.isAuthenticated = await authClient.isAuthenticated();
           const principal = await authClient.getIdentity()?.getPrincipal().toText();
