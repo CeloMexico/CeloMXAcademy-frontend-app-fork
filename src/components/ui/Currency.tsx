cae38d430e1db38447cfc8fc3e3af5d19ec3b958
SolDAO <aszalvarez@gmail.com>
2025-06-08 20:51:05 -0600

    fix: remove unused useMemo import from Currency.tsx

--- src/components/ui/Currency.tsx
+++ src/components/ui/Currency.tsx
@@ -1,5 +1,5 @@
-import { formatCurrency } from "@coingecko/cryptoformat";
-import { ReactElement, useMemo } from "react";
+// import { formatCurrency } from "@coingecko/cryptoformat";
+import { ReactElement /*, useMemo */ } from "react";
 
 /**
  * Interface for currency props
@@ -26,15 +26,15 @@ interface CurrencyProps {
  * @returns {ReactElement}
  */
 export default function Currency({ value = 0, token = "", testId = "currencyId" }: CurrencyProps): ReactElement {
-  const currency = useMemo(() => {
-    return formatCurrency(value, token, "en", true, {
-      decimalPlaces: 2,
-      significantFigures: 3,
-    });
-  }, [token, value]);
+  // const currency = useMemo(() => {
+  //   return formatCurrency(value, token, "en", true, {
+  //     decimalPlaces: 2,
+  //     significantFigures: 3,
+  //   });
+  // }, [token, value]);
   return (
     <span data-testid={testId} className="whitespace-nowrap">
-      {currency} {token}
+      {value} {token}
     </span>
   );
 }
